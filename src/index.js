d4f4c1dbb88fdf46456eeadbe9f9bff4361195cd
Teja <teja14950@gmail.com>
2026-04-18 08:26:49 +0530

    feat: connect PostgreSQL databse and add test end point

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const pool = require("./db");
 const app = express();
 const PORT = 3000;
 
@@ -7,6 +7,15 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
+});
+app.get("/test-db",async(req,res)=>{
+  try{
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (err){
+    console.error(err);
+    res.status(500).send("Database error");
+  }
 });
 
 app.listen(PORT, () => {
